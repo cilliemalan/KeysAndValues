f7dafd65d43691273781ef9222a47a39b7284753
Cillie Malan <CillieM@eurekadiy.co.za>
2025-05-23 13:12:15 +0200

    kind of stub string to byte conversion

---
+++ KeysAndValues/Internal/StringToBytesConversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeysAndValues.Internal
+{
+    internal static class StringToBytesConversion
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mem GetBytes(string str)
+        {
+            return new Mem(Encoding.UTF8.GetBytes(str).AsMemory());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string GetString(Mem mem)
+        {
+            return Encoding.UTF8.GetString(mem.Span);
+        }
+    }
+}
