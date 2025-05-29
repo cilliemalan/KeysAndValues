8b9cdd54b67c68fb372629b786d2e9e5ba5787fd
Cillié Malan <cilliemalan@gmail.com>
2025-05-29 12:52:05 +0200

    Use corpus as linked file

--- KeysAndValues.Tests/AvlTreeTests.cs
+++ KeysAndValues.Tests/AvlTreeTests.cs
@@ -1,6 +1,4 @@
-﻿using BenchmarkDotNet.Disassemblers;
-using Microsoft.Diagnostics.Tracing;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
--- KeysAndValues.Tests/MemTests.cs
+++ KeysAndValues.Tests/MemTests.cs
@@ -1,5 +1,4 @@
-﻿using KeysAndValues.Bench;
-using System.Text;
+﻿using System.Text;
 
 namespace KeysAndValues.Tests;
 
