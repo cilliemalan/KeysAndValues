025d7f20a6b916b4686b7c4937af2e2f9bae2fac
Cillié Malan <cilliemalan@gmail.com>
2025-05-29 12:54:06 +0200

    Added Deserialize method

--- KeysAndValues/KeyValueStore.Serialization.cs
+++ KeysAndValues/KeyValueStore.Serialization.cs
@@ -33,4 +33,80 @@ public partial class KeyValueStore
         sha.GetCurrentHash(tmp);
         stream.Write(tmp);
     }
+
+    public static KeyValueStore Deserialize(Stream stream)
+    {
+        if (!TryDeserialize(stream, out var kvs))
+        {
+            throw new InvalidDataException("Invalid KeyValueStore serialization data.");
+        }
+        return kvs;
+    }
+
+    public static bool TryDeserialize(Stream stream, [MaybeNullWhen(false)] out KeyValueStore kvs)
+    {
+        kvs = null;
+        using var sha = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
+        Span<byte> tmp = stackalloc byte[32];
+        if (!stream.TryReadExactly(tmp[..12]))
+        {
+            return false;
+        }
+        sha.AppendData(tmp[..12]);
+        long sequence = BitConverter.ToInt64(tmp);
+        int count = BitConverter.ToInt32(tmp[8..]);
+        if (sequence < 0 || count < 0 ||
+            !(count > 0 || sequence == 0))
+        {
+            return false;
+        }
+
+        var builder = ImmutableAvlTree<Mem, Mem>.Empty.ToBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            if (!stream.TryReadExactly(tmp[..4]))
+            {
+                return false;
+            }
+            sha.AppendData(tmp[..4]);
+            int keyLength = BitConverter.ToInt32(tmp);
+            var keyMem = new byte[keyLength];
+            if (!stream.TryReadExactly(keyMem))
+            {
+                return false;
+            }
+            sha.AppendData(keyMem);
+            if (!stream.TryReadExactly(tmp[..4]))
+            {
+                return false;
+            }
+            sha.AppendData(tmp[..4]);
+            int valueLength = BitConverter.ToInt32(tmp);
+            var valueMem = new byte[valueLength];
+            if (!stream.TryReadExactly(valueMem))
+            {
+                return false;
+            }
+            sha.AppendData(valueMem);
+            builder.Add(new(keyMem), new(valueMem));
+        }
+
+        sha.GetCurrentHash(tmp);
+        Span<byte> tmp2 = stackalloc byte[32];
+        if (!stream.TryReadExactly(tmp2))
+        {
+            return false;
+        }
+        if (!tmp.SequenceEqual(tmp2))
+        {
+            return false;
+        }
+
+        kvs = new()
+        {
+            store = builder.ToImmutable(),
+            sequence = sequence
+        };
+        return true;
+    }
 }
