01b6200c9b926d23b0c909a89028daf8f860781b
Cillié Malan <cilliemalan@gmail.com>
2025-05-29 12:53:52 +0200

    Not using old sha

--- KeysAndValues/KeyValueStore.Serialization.cs
+++ KeysAndValues/KeyValueStore.Serialization.cs
@@ -9,28 +9,28 @@ public partial class KeyValueStore
     {
         var snap = Snapshot(out var sequence);
 
-        using var sha = new Sha256Hasher();
+        using var sha = IncrementalHash.CreateHash(HashAlgorithmName.SHA256);
         Span<byte> tmp = stackalloc byte[32];
         BitConverter.TryWriteBytes(tmp, sequence);
         BitConverter.TryWriteBytes(tmp[8..], snap.Count);
-        stream.Write(tmp[..16]);
-        sha.Ingest(tmp[..16]);
+        stream.Write(tmp[..12]);
+        sha.AppendData(tmp[..12]);
 
         foreach (var node in snap)
         {
             BitConverter.TryWriteBytes(tmp[0..4], node.Key.Length);
             BitConverter.TryWriteBytes(tmp[4..8], node.Value.Length);
             stream.Write(tmp[0..4]);
-            sha.Ingest(tmp[0..4]);
+            sha.AppendData(tmp[0..4]);
             stream.Write(node.Key.Span);
-            sha.Ingest(node.Key.Span);
+            sha.AppendData(node.Key.Span);
             stream.Write(tmp[4..8]);
-            sha.Ingest(tmp[4..8]);
+            sha.AppendData(tmp[4..8]);
             stream.Write(node.Value.Span);
-            sha.Ingest(node.Value.Span);
+            sha.AppendData(node.Value.Span);
         }
 
-        sha.Compute(tmp);
+        sha.GetCurrentHash(tmp);
         stream.Write(tmp);
     }
 }
