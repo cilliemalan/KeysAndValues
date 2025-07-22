0a5d7a24fcf23e2fb2e2684cea4aaa1910cab712
Cillie Malan <CillieM@eurekadiy.co.za>
2025-07-22 12:32:29 +0200

    More cleaning

--- KeysAndValues.Tests/KeyValueStoreSerializeTest.cs
+++ KeysAndValues.Tests/KeyValueStoreSerializeTest.cs
@@ -1,34 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace KeysAndValues.Tests;
 
-namespace KeysAndValues.Tests
+public class KeyValueStoreSerializeTest
 {
-    public class KeyValueStoreSerializeTest
+    [Fact]
+    public void BasicSerializeTest()
     {
-        [Fact]
-        public void BasicSerializeTest()
-        {
-            var kvs = new KeyValueStore(1, Corpus.GenerateUnsorted(10));
-            using var ms = new MemoryStream();
-            KeyValueStoreSerialization.SerializeStoreVersion(kvs.Snapshot(), ms);
-            Debug.Assert(ms.Length > 0);
-        }
+        var kvs = new KeyValueStore(1, Corpus.GenerateUnsorted(10));
+        using var ms = new MemoryStream();
+        KeyValueStoreSerialization.SerializeStoreVersion(kvs.Snapshot(), ms);
+        Assert.True(ms.Length > 0);
+    }
 
-        [Fact]
-        public void BasicDeserializeTest()
-        {
-            var kvs = new KeyValueStore(1, Corpus.GenerateUnsorted(10));
-            using var ms = new MemoryStream();
-            KeyValueStoreSerialization.SerializeStoreVersion(kvs.Snapshot(), ms);
-            ms.Position = 0;
-            var ver2 = KeyValueStoreSerialization.DeserializeStoreVersion(ms);
-            Assert.Equal(kvs.Count, ver2.Data.Count);
-            Assert.Equal(kvs.Sequence, ver2.Sequence);
-            Assert.Equal(kvs.Data.AsEnumerable(), ver2.Data.AsEnumerable());
-        }
+    [Fact]
+    public void BasicDeserializeTest()
+    {
+        var kvs = new KeyValueStore(1, Corpus.GenerateUnsorted(10));
+        using var ms = new MemoryStream();
+        KeyValueStoreSerialization.SerializeStoreVersion(kvs.Snapshot(), ms);
+        ms.Position = 0;
+        var ver2 = KeyValueStoreSerialization.DeserializeStoreVersion(ms);
+        Assert.Equal(kvs.Count, ver2.Data.Count);
+        Assert.Equal(kvs.Sequence, ver2.Sequence);
+        Assert.Equal(kvs.Data.AsEnumerable(), ver2.Data.AsEnumerable());
     }
 }
