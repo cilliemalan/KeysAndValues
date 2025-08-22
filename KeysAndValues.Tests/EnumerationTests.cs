e247b55290335562118704a42b892e300d94c5d0
Cillié Malan <cilliemalan@gmail.com>
2025-08-22 11:01:50 +0200

    More tests

--- KeysAndValues.Tests/EnumerationTests.cs
+++ KeysAndValues.Tests/EnumerationTests.cs
@@ -5,7 +5,7 @@ public class EnumerationTests
     [Fact]
     public void BasicEnumerationTest()
     {
-        var store = new KeyValueStore(1, new Dictionary<Mem,Mem>
+        var store = new KeyValueStore(1, new Dictionary<Mem, Mem>
         {
             ["key1"] = "value1",
             ["key2"] = "value2",
@@ -38,6 +38,63 @@ public class EnumerationTests
         Assert.Contains(new KeyValuePair<Mem, Mem>("key2", "value2"), list);
         Assert.Contains(new KeyValuePair<Mem, Mem>("key3", "value3"), list);
         Assert.Contains(new KeyValuePair<Mem, Mem>("key4", "value4"), list);
+    }
+
+    [Fact]
+    public void OnlyLowerBoundRangeEnumerationTest()
+    {
+        var store = new KeyValueStore(1, new Dictionary<Mem, Mem>
+        {
+            ["key1"] = "value1",
+            ["key2"] = "value2",
+            ["key3"] = "value3",
+            ["key4"] = "value4",
+            ["key5"] = "value5"
+        });
+        var list = store.Enumerate("key2").ToList();
+        Assert.Equal(4, list.Count);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key2", "value2"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key3", "value3"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key4", "value4"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key5", "value5"), list);
+    }
+
+    [Fact]
+    public void EnumeratePrefixWithoutPrefixTest()
+    {
+        var store = new KeyValueStore(1, new Dictionary<Mem, Mem>
+        {
+            ["key1"] = "value1",
+            ["key2"] = "value2",
+            ["key3"] = "value3",
+            ["key4"] = "value4",
+            ["key5"] = "value5"
+        });
+        var list = store.EnumeratePrefix("").ToList();
+        Assert.Equal(5, list.Count);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key1", "value1"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key2", "value2"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key3", "value3"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key4", "value4"), list);
+        Assert.Contains(new KeyValuePair<Mem, Mem>("key5", "value5"), list);
+    }
+
+    [Fact]
+    public void PrefixEnumerateLimitTest()
+    {
+        var store = new KeyValueStore();
+        store.Set(new byte[] { 0xff, 0xff, 0xfe, 0x1 }, "nope");
+        store.Set(new byte[] { 0xff, 0xff, 0xff, 0x1 }, "one");
+        store.Set(new byte[] { 0xff, 0xff, 0xff, 0x2 }, "two");
+        store.Set(new byte[] { 0xff, 0xff, 0xff, 0x3 }, "three");
+
+        var list = store.EnumeratePrefix(new byte[] { 0xff, 0xff, 0xff })
+            .Select(x => (string)x.Value)
+            .ToList();
+        Assert.Equal(3, list.Count);
+        Assert.Equal("one", list[0]);
+        Assert.Equal("two", list[1]);
+        Assert.Equal("three", list[2]);
     }
 
     [Fact]
