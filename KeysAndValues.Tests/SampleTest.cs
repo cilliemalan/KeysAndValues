a2ac8e50856caea106fca3aca9b87657412af835
Cillié Malan <cilliemalan@gmail.com>
2025-08-22 12:18:03 +0200

    Sample test

---
+++ KeysAndValues.Tests/SampleTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace KeysAndValues.Tests;
+
+public class SampleTest
+{
+    [Fact]
+    public void RunSampleTest()
+    {
+        Assembly.Load("KeysAndValues.Sample")
+            .GetType("Program")!
+            .GetMethods(BindingFlags.Static | BindingFlags.NonPublic)
+            .First()
+            .Invoke(null, [Array.Empty<string>()]);
+    }
+}
